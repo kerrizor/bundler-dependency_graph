ab26bec19f881494c5a3581564e849c95ececf57
Kerri Miller <kerrizor@github.com>
2017-02-17 10:24:55 -0800

    Add basic Bundler::API::Plugin structure for testing

--- lib/bundler/dependency_graph.rb
+++ lib/bundler/dependency_graph.rb
@@ -2,6 +2,12 @@ require "bundler/dependency_graph/version"
 
 module Bundler
   module DependencyGraph
-    # Your code goes here...
+    class GraphBuilder
+      Bundler::Plugin::API.command "graph", self
+
+      def exec(command, args)
+        puts "Hi Kerri & Whitney!"
+      end
+    end
   end
 end
