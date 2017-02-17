8014b7c87b9f09a281e4bed3e5f0a09ca00b556e
Kerri Miller <kerrizor@github.com>
2017-02-17 10:06:30 -0800

    Initial commit

---
+++ lib/bundler/dependency_graph.rb
@@ -0,0 +1,7 @@
+require "bundler/dependency_graph/version"
+
+module Bundler
+  module DependencyGraph
+    # Your code goes here...
+  end
+end
---
+++ lib/bundler/dependency_graph/version.rb
@@ -0,0 +1,5 @@
+module Bundler
+  module DependencyGraph
+    VERSION = "0.1.0"
+  end
+end
