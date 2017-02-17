96825465fc51fa0ab38d4e60606ce32d4ce3eb44
Kerri Miller <kerrizor@github.com>
2017-02-17 10:51:48 -0800

    Pull GraphBuilder into its own file

---
+++ lib/bundler/dependency_graph/graph_builder.rb
@@ -0,0 +1,11 @@
+module Bundler
+  module DependencyGraph
+    class GraphBuilder
+      Bundler::Plugin::API.command "graph", self
+
+      def exec(command, args)
+        puts "Currently unimplented; use `bundle viz` to generate your graphs for now."
+      end
+    end
+  end
+end
